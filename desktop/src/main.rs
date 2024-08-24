a78f2df774f61521da57f9391b744a5a8021dcd7
scsteinhauser <scsteinhauser@gmail.com>
2024-08-24 14:20:20 -0700

    Set up gameloop and load screen with cargo run test

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use chip8_core::*;
 
+use sdl2::event::Event;
+
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE; 
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
@@ -24,4 +26,17 @@ fn main() {
     let mut canvas = window.into_canvas().present_vsync().build().unwrap(); 
     canvas.clear();
     canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                },
+                _ => ()
+            }
+        }
+    }
 }
